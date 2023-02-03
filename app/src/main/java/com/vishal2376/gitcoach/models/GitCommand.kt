39119a67ecbfb1aabc057db8bf1d65fb9bbf3314
Vishal Singh <vishalsingh2376@gmail.com>
2023-02-03 19:44:15 +0530

    add : data class for git command

---
+++ app/src/main/java/com/vishal2376/gitcoach/models/GitCommand.kt
@@ -0,0 +1,9 @@
+package com.vishal2376.gitcoach.models
+
+data class GitCommand(
+    val name: String,
+    val command: String,
+    val syntax: String,
+    val description: String,
+    val example: String
+)
