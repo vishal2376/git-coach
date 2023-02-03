bf812fca1c552371674949c0245f329758a5c8bc
Vishal Singh <vishalsingh2376@gmail.com>
2023-02-03 23:21:32 +0530

    access json file

--- app/src/main/java/com/vishal2376/gitcoach/MainActivity.kt
+++ app/src/main/java/com/vishal2376/gitcoach/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.vishal2376.gitcoach
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.AssetManager
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.vishal2376.gitcoach.models.GitCommand
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //temp
+        val gson = Gson()
+        val jsonString = assets.readFile("git_commands.json")
+        val gitCommandList = gson.fromJson(jsonString, GitCommand::class.java)
+
+        Log.e("@@@", gitCommandList.toString())
+
     }
+
+    private fun AssetManager.readFile(fileName: String) = open(fileName)
+        .bufferedReader()
+        .use { it.readText() }
 }
---
+++ app/src/main/java/com/vishal2376/gitcoach/models/GitCommandItem.kt
@@ -0,0 +1,9 @@
+package com.vishal2376.gitcoach.models
+
+data class GitCommandItem(
+    val name: String,
+    val command: String,
+    val syntax: String,
+    val description: String,
+    val example: String
+)
