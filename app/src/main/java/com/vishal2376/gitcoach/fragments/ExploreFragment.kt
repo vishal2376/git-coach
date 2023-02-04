5693508d45527a01c141ec6ecaf71dc6dc397fae
Vishal Singh <vishalsingh2376@gmail.com>
2023-02-04 22:55:18 +0530

    add : viewbinding enabled + setup

---
+++ app/src/main/java/com/vishal2376/gitcoach/fragments/ExploreFragment.kt
@@ -0,0 +1,35 @@
+package com.vishal2376.gitcoach.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vishal2376.gitcoach.databinding.FragmentExploreBinding
+
+class ExploreFragment : Fragment() {
+
+    private var _binding: FragmentExploreBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
