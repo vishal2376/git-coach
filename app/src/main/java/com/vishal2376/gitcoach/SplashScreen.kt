2d459cd51d83983d765130123f95eea2d3aca010
Vishal Singh <vishalsingh2376@gmail.com>
2023-02-22 23:36:00 +0530

    add : splash screen

---
+++ app/src/main/java/com/vishal2376/gitcoach/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.vishal2376.gitcoach
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.vishal2376.gitcoach.utils.LoadSettings
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //load settings
+        LoadSettings.loadTheme(this)
+
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+    }
+}
