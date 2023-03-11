f758403e304c25c3908d490f10994c8ffc0d4b7c
Vishal & Singh <vishalsingh2376@gmail.com>
2023-03-11 16:58:10 +0530

    update : implement share , reportbug,suggestion , share app button

--- app/src/main/java/com/vishal2376/gitcoach/MainActivity.kt
+++ app/src/main/java/com/vishal2376/gitcoach/MainActivity.kt
@@ -1,17 +1,91 @@
 package com.vishal2376.gitcoach
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import com.vishal2376.gitcoach.databinding.ActivityMainBinding
+import com.vishal2376.gitcoach.utils.Constants
+import com.vishal2376.gitcoach.utils.Constants.shareMessage
 import com.vishal2376.gitcoach.utils.LoadSettings
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //load settings
         LoadSettings.loadTheme(this)
 
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.itemReportBug -> {
+                    reportBug()
+                }
+                R.id.itemSuggestions -> {
+                    getSuggestions()
+                }
+                R.id.itemRateUs -> {
+                    rateUs()
+                }
+                R.id.itemShareApp -> {
+                    shareApp()
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun shareApp() {
+        val shareIntent = Intent(Intent.ACTION_SEND);
+        shareIntent.type = "text/plain";
+        Constants.shareMessage += "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Git Coach");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+        startActivity(Intent.createChooser(shareIntent, "Share This App"));
+    }
+
+    private fun reportBug() {
+        val subject = "Git Coach: Report Bug"
+        val uriBuilder = StringBuilder("mailto:" + Uri.encode(Constants.email))
+        uriBuilder.append("?subject=" + Uri.encode(subject))
+        val uriString = uriBuilder.toString()
+
+        val intent = Intent(Intent.ACTION_SENDTO, Uri.parse(uriString))
+        startActivity(Intent.createChooser(intent, "Report Bug"))
+    }
+
+    private fun getSuggestions() {
+
+        val subject = "Git Coach: Suggestions"
+        val uriBuilder = StringBuilder("mailto:" + Uri.encode(Constants.email))
+        uriBuilder.append("?subject=" + Uri.encode(subject))
+        val uriString = uriBuilder.toString()
+
+        val intent = Intent(Intent.ACTION_SENDTO, Uri.parse(uriString))
+        startActivity(Intent.createChooser(intent, "Send Suggestions"))
+    }
+
+    private fun rateUs() {
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("http://play.google.com/store/apps/details?id=" + applicationContext.packageName)
+        )
+        startActivity(intent)
 
     }
 
---
+++ app/src/main/java/com/vishal2376/gitcoach/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.vishal2376.gitcoach.utils
+
+object Constants {
+    const val email = "vishalsingh2376@gmail.com"
+    var shareMessage =
+        "Become a Git expert with Git Coach! Learn Git and GitHub commands with ease. Download now\n";
+
+}
