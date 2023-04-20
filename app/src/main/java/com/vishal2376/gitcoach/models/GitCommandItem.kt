c032e034e201327b8ab3585c57707201892614d9
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-20 15:53:19 +0530

    fix: crash due to typo

--- app/src/main/java/com/vishal2376/gitcoach/models/GitCommandItem.kt
+++ app/src/main/java/com/vishal2376/gitcoach/models/GitCommandItem.kt
@@ -1,9 +1,5 @@
 package com.vishal2376.gitcoach.models
 
 data class GitCommandItem(
-    val name: String,
-    val command: String,
-    val syntax: String,
-    val description: String,
-    val example: String
+    val name: String, val command: String, val description: String, val example: String
 )
