c09f6a08fbe417a57b355f937e2aecc3449a27d8
vishal2376 <vishalsingh2376@gmail.com>
2023-07-04 22:25:37 +0530

    update: add temp ideas for community tab

--- app/src/main/java/com/vishal2376/gitcoach/fragments/CommunityFragment.kt
+++ app/src/main/java/com/vishal2376/gitcoach/fragments/CommunityFragment.kt
@@ -1,5 +1,7 @@
 package com.vishal2376.gitcoach.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +21,18 @@ class CommunityFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentCommunityBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvDiscussionLink.setOnClickListener {
+            val discussionLink = "https://github.com/vishal2376/git-coach/issues/5"
+            val intent = Intent(
+                Intent.ACTION_VIEW, Uri.parse(discussionLink)
+            )
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
