a4cbb9cb823dc9c2c335295aec8731aadc04f073
vishal2376 <vishalsingh2376@gmail.com>
2023-08-14 16:28:07 +0530

    update: impl font size settings

--- app/src/main/java/com/vishal2376/gitcoach/fragments/FontSettingFragment.kt
+++ app/src/main/java/com/vishal2376/gitcoach/fragments/FontSettingFragment.kt
@@ -1,20 +1,53 @@
 package com.vishal2376.gitcoach.fragments
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vishal2376.gitcoach.R
+import com.vishal2376.gitcoach.databinding.FragmentFontSettingBinding
+import com.vishal2376.gitcoach.utils.Constants
 
 class FontSettingFragment : Fragment() {
+
+    private var _binding: FragmentFontSettingBinding? = null
+    private val binding get() = _binding!!
+
+    private var titleSize: Float = Constants.FONT_SIZE_TITLE
+    private var descriptionSize: Float = Constants.FONT_SIZE_DESCRIPTION
+    private var commandSize: Float = Constants.FONT_SIZE_COMMAND
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_font_setting, container, false)
+        _binding = FragmentFontSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        handleInputs()
+
+    }
+
+    private fun handleInputs() {
+        binding.sliderTitle.addOnChangeListener { _, value, _ ->
+            titleSize = Constants.FONT_SIZE_TITLE + value
+            binding.tvGitName.setTextSize(TypedValue.COMPLEX_UNIT_SP, titleSize)
+        }
+        binding.sliderDescription.addOnChangeListener { _, value, _ ->
+            descriptionSize = Constants.FONT_SIZE_DESCRIPTION + value
+            binding.tvGitDescription.setTextSize(TypedValue.COMPLEX_UNIT_SP, descriptionSize)
+            binding.tvGitExample.setTextSize(TypedValue.COMPLEX_UNIT_SP, descriptionSize)
+        }
+        binding.sliderCommand.addOnChangeListener { _, value, _ ->
+            commandSize = Constants.FONT_SIZE_COMMAND + value
+            binding.tvGitCommand.setTextSize(TypedValue.COMPLEX_UNIT_SP, commandSize)
+        }
     }
 
 }
