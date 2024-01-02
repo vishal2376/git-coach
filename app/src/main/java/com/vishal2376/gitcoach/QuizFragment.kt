ab6ad11ee77d113fdc708f92afc6ca6db7b9ed48
Vishal Singh <vishalsingh2376@gmail.com>
2024-01-02 23:05:27 +0530

    add: quiz fragment

---
+++ app/src/main/java/com/vishal2376/gitcoach/QuizFragment.kt
@@ -0,0 +1,41 @@
+package com.vishal2376.gitcoach
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vishal2376.gitcoach.databinding.FragmentQuizBinding
+import com.vishal2376.gitcoach.utils.LoadSettings
+
+
+class QuizFragment : Fragment() {
+
+	private var _binding: FragmentQuizBinding? = null
+	private val binding get() = _binding!!
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		//load settings
+		LoadSettings.loadTheme(requireContext())
+
+		// Inflate the layout for this fragment
+		_binding = FragmentQuizBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+
+	override fun onViewCreated(
+		view: View,
+		savedInstanceState: Bundle?
+	) {
+		super.onViewCreated(view, savedInstanceState)
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
+}
