43fb75fa5dccf8b4f48ade833e7158503f1bf7bb
vishal2376 <vishalsingh2376@gmail.com>
2024-01-18 22:23:20 +0530

    chore: add func to load git quiz json

--- app/src/main/java/com/vishal2376/gitcoach/utils/LoadData.kt
+++ app/src/main/java/com/vishal2376/gitcoach/utils/LoadData.kt
@@ -5,6 +5,7 @@ import android.content.res.AssetManager
 import com.google.gson.Gson
 import com.vishal2376.gitcoach.models.GitCommand
 import com.vishal2376.gitcoach.models.lesson.GitLesson
+import com.vishal2376.gitcoach.models.quiz.GitQuiz
 
 object LoadData {
 
@@ -18,6 +19,13 @@ object LoadData {
         val locale = LoadSettings.getLocale(context)
         val jsonString = context.assets.readFile("$locale/git_lessons.json")
         return Gson().fromJson(jsonString, GitLesson::class.java)
+    }
+
+    fun getGitQuizData(context: Context): GitQuiz? {
+//        val locale = LoadSettings.getLocale(context)
+        val locale = "en" // todo: remove it after translations
+        val jsonString = context.assets.readFile("$locale/git_quiz.json")
+        return Gson().fromJson(jsonString, GitQuiz::class.java)
     }
 
     private fun AssetManager.readFile(fileName: String) = open(fileName)
