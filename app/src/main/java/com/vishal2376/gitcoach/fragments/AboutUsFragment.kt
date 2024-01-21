0ce36ac3b2b7c448342c7338c0c68c0c4c5eb5f1
vishal2376 <vishalsingh2376@gmail.com>
2024-01-21 14:10:29 +0530

    feat: add about us page

---
+++ app/src/main/java/com/vishal2376/gitcoach/fragments/AboutUsFragment.kt
@@ -0,0 +1,36 @@
+package com.vishal2376.gitcoach.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vishal2376.gitcoach.MainActivity
+import com.vishal2376.gitcoach.databinding.FragmentAboutUsBinding
+
+class AboutUsFragment : Fragment() {
+
+    private var _binding: FragmentAboutUsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentAboutUsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        MainActivity.appBarLayout.visibility = View.GONE
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        MainActivity.appBarLayout.visibility = View.VISIBLE
+        _binding = null
+    }
+
+}
