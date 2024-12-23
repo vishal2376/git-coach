6d4b6b9cf3143d698a99357796346d578211cb27
vishal2376 <vishalsingh2376@gmail.com>
2024-12-23 20:03:38 +0530

    feat: Translate data only if it's translation files are available

--- app/src/main/java/com/vishal2376/gitcoach/utils/LoadData.kt
+++ app/src/main/java/com/vishal2376/gitcoach/utils/LoadData.kt
@@ -11,21 +11,44 @@ object LoadData {
 
     fun getGitCommandData(context: Context): GitCommand? {
         val locale = LoadSettings.getLocale(context)
-        val jsonString = context.assets.readFile("$locale/git_commands.json")
+        val fileName = "$locale/git_commands.json"
+        val jsonString = if (isAssetAvailable(context, fileName)) {
+            context.assets.readFile(fileName)
+        } else {
+            context.assets.readFile("en/git_commands.json")
+        }
         return Gson().fromJson(jsonString, GitCommand::class.java)
     }
 
     fun getGitLessonData(context: Context): GitLesson? {
         val locale = LoadSettings.getLocale(context)
-        val jsonString = context.assets.readFile("$locale/git_lessons.json")
+        val fileName = "$locale/git_lessons.json"
+        val jsonString = if (isAssetAvailable(context, fileName)) {
+            context.assets.readFile(fileName)
+        } else {
+            context.assets.readFile("en/git_lessons.json")
+        }
         return Gson().fromJson(jsonString, GitLesson::class.java)
     }
 
     fun getGitQuizData(context: Context): GitQuiz? {
-//        val locale = LoadSettings.getLocale(context)
-        val locale = "en" // todo: remove it after translations
-        val jsonString = context.assets.readFile("$locale/git_quiz.json")
+        val locale = LoadSettings.getLocale(context)
+        val fileName = "$locale/git_quiz.json"
+        val jsonString = if (isAssetAvailable(context, fileName)) {
+            context.assets.readFile(fileName)
+        } else {
+            context.assets.readFile("en/git_quiz.json")
+        }
         return Gson().fromJson(jsonString, GitQuiz::class.java)
+    }
+
+    private fun isAssetAvailable(context: Context, fileName: String): Boolean {
+        return try {
+            context.assets.open(fileName).close()
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
     private fun AssetManager.readFile(fileName: String) = open(fileName)
